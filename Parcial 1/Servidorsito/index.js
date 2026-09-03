6591bd183157189ce1fbf6ee6642bec531e3dff0
gaelonofre224 <gaelonofre117@gmail.com>
2026-09-03 08:19:31 -0500

    middleware y router

--- Parcial 1/Servidorsito/index.js
+++ Parcial 1/Servidorsito/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const recursoRouter = require('./routes/recursoRouter');
+const Router = require('./Router.js');
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/monstruos', recursoRouter.router);
+const registrarPeticion = (req, res, next) => {
+  console.log(`[LOG] Petición recibida -> Método: ${req.method} | Ruta: ${req.url}`);
+  next();
+};
+
+app.use(registrarPeticion);
+
+app.use('/monstruos', Router.router);
 
 app.listen(8082, function(err) {
   if (err) console.log(err);
