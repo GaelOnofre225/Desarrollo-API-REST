1c77d6b23ca5cf8cf24156d114dca141ab72eab5
gaelonofre224 <gaelonofre117@gmail.com>
2026-09-04 07:12:47 -0500

    2

--- Parcial 1/Servidorsito/index.js
+++ Parcial 1/Servidorsito/index.js
@@ -1,16 +1,19 @@
 const express = require('express');
+const morgan = require('morgan'); 
+const cors = require('cors');     
 const Router = require('./Router.js');
 
 const app = express();
 
+app.use(cors());          
+app.use(morgan('dev'));   
+
 app.use(express.json());
 
-const registrarPeticion = (req, res, next) => {
-  console.log(`[LOG] Petición recibida -> Método: ${req.method} | Ruta: ${req.url}`);
-  next();
-};
-
-app.use(registrarPeticion);
+app.use((req, res, next) => {
+  console.log(`[Middleware de Aplicación] Petición entrante: ${req.method} a ${req.url} - Fecha: ${new Date().toLocaleTimeString()}`);
+  next(); 
+});
 
 app.use('/monstruos', Router.router);
 
@@ -18,3 +21,6 @@ app.listen(8082, function(err) {
   if (err) console.log(err);
   console.log("Servidor escuchando en puerto 8082");
 });
+
+//El servidor debe de poder recibir archivos como jpg
+//Separar por carpetas los archivos
