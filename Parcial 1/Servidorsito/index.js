696de643b3944a1b99a4d3602dff1df984aec72d
gaelonofre224 <gaelonofre117@gmail.com>
2026-09-10 08:21:23 -0500

    Vista

--- Parcial 1/Servidorsito/index.js
+++ Parcial 1/Servidorsito/index.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const morgan = require('morgan'); 
 const cors = require('cors');     
-const Router = require('./Router.js');
+const path = require('path');
+const Router = require('./routes/Router');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'Vistas'));
+
 app.use(cors());          
 app.use(morgan('dev'));   
-
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(`[Middleware de Aplicación] Petición entrante: ${req.method} a ${req.url} - Fecha: ${new Date().toLocaleTimeString()}`);
+  console.log(`[LOG] Petición entrante: ${req.method} a ${req.url} - Fecha: ${new Date().toLocaleTimeString()}`);
   next(); 
+});
+
+app.get('/ruta', (req, res, next) => {
+  let opciones = {
+    titulo: "Monster Hunter",
+    subtitulo: "Monstruopedia"
+  };
+  res.render('plantilla', opciones);
 });
 
 app.use('/monstruos', Router.router);
@@ -21,6 +32,3 @@ app.listen(8082, function(err) {
   if (err) console.log(err);
   console.log("Servidor escuchando en puerto 8082");
 });
-
-//El servidor debe de poder recibir archivos como jpg
-//Separar por carpetas los archivos
